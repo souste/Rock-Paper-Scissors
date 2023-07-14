e434abeb1f65a09c8ebbec756e376694ef8d750e
Stephen <stephensouness@outlook.com>
2023-07-14 19:32:57 +0100

    almost complete, just need to stop computer selection repetition in game() loop

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ function getComputerChoice() {
   return answer;
 }
 
+let playerScore = 0;
+let compScore = 0;
+
 function playRound(playerSelection, computerSelection) {
   const lowerCase = playerSelection.toLowerCase();
   const finalCase = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
@@ -14,30 +17,45 @@ function playRound(playerSelection, computerSelection) {
     (finalCase === "Paper" && computerSelection === "Scissors") ||
     (finalCase === "Scissors" && computerSelection === "Rock")
   ) {
-    return `You lose! ${computerSelection} beats ${finalCase}`;
+    playerScore += -1;
+    compScore += 1;
+    return `You lose! ${computerSelection} beats ${finalCase}. playerScore is ${playerScore}. compScore is ${compScore}`;
   } else if (
     (finalCase === "Rock" && computerSelection === "Scissors") ||
     (finalCase === "Paper" && computerSelection === "Rock") ||
     (finalCase === "Scissors" && computerSelection === "Paper")
   ) {
-    return `You win! ${finalCase} beats ${computerSelection}`;
+    playerScore += 1;
+    compScore += -1;
+    return `You win! ${finalCase} beats ${computerSelection}. playerScore is ${playerScore}. compScore is ${compScore}`;
   } else if (finalCase === computerSelection) {
-    return `You draw!`;
+    return `You draw! playerScore is ${playerScore}. compScore is ${compScore}`;
   } else {
+    playerScore += 0;
+    compScore += 0;
     return `Please enter Rock, Paper or Scissors`;
   }
 }
-
+let playerSelection = prompt("Go!", "");
 const computerSelection = getComputerChoice();
 
-let score = 0;
-let compScore = 0;
+console.log(playRound(playerSelection, computerSelection));
 
 function game() {
-  for (let i = 1; i <= 5; i++) {
+  for (let i = 1; i < 5; i++) {
     let playerSelection = prompt("Go!", "");
     console.log(playRound(playerSelection, computerSelection));
+  }
+  if (playerScore > compScore) {
+    console.log("Well done, you win");
+  } else if (compScore > playerScore) {
+    console.log("Bad luck, you lose");
   }
 }
 
 game();
+
+// CURRENT ISSUES - EVERYTHING WORKS BEFORE THE FINAL PART OF THE PROBLEM
+// Computer selection remains the same over 5 turns - this is the final problem, everything works when I solve this!
+
+// CAN TRY USING A DEBUGGER
