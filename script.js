95eddf603226f5800a72f14c71790b6366ed0097
Stephen <stephensouness@outlook.com>
2023-07-26 15:23:37 +0100

    player choice now determined by buttons AND result displayed within the UI

--- script.js
+++ script.js
@@ -9,48 +9,53 @@ let playerScore = 0;
 let compScore = 0;
 
 function playRound(playerSelection, computerSelection) {
-  const lowerCase = playerSelection.toLowerCase();
-  const finalCase = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
-
   if (
-    (finalCase === "Rock" && computerSelection === "Paper") ||
-    (finalCase === "Paper" && computerSelection === "Scissors") ||
-    (finalCase === "Scissors" && computerSelection === "Rock")
+    (playerSelection === "Rock" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Rock")
   ) {
     playerScore += -1;
     compScore += 1;
-    return `You lose! ${computerSelection} beats ${finalCase}.`;
+    return (result.innerText = `You lose! ${computerSelection} beats ${playerSelection}.`);
   } else if (
-    (finalCase === "Rock" && computerSelection === "Scissors") ||
-    (finalCase === "Paper" && computerSelection === "Rock") ||
-    (finalCase === "Scissors" && computerSelection === "Paper")
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")
   ) {
     playerScore += 1;
     compScore += -1;
-    return `You win! ${finalCase} beats ${computerSelection}. `;
-  } else if (finalCase === computerSelection) {
+    return (result.innerText = `You win! ${playerSelection} beats ${computerSelection}. `);
+  } else if (playerSelection === computerSelection) {
     playerScore += 0;
     compScore += 0;
-    return `You draw!`;
-  } else {
-    // can remove this bit when buttons are created
-    playerScore += 0;
-    compScore += 0;
-    return `Please enter Rock, Paper or Scissors`;
+    return (result.innerText = `You draw!`);
   }
 }
 
-function game() {
-  for (let i = 1; i <= 5; i++) {
-    let playerSelection = prompt("Go!", "");
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let playerSelection = button.id;
     let computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-  }
-  if (playerScore > compScore) {
-    console.log("WELL DONE, YOU WIN THE GAME!!");
-  } else if (compScore > playerScore) {
-    console.log("BAD LUCK, COMPUTER WINS THE GAME");
-  }
-}
 
-game();
+    console.log(playRound(playerSelection, computerSelection));
+  });
+});
+
+const result = document.querySelector(".result");
+const score = document.querySelector(".score");
+
+// function game() {
+//   for (let i = 1; i <= 5; i++) {
+//     let playerSelection = prompt("Go!", "");
+//     let computerSelection = getComputerChoice();
+//     console.log(playRound(playerSelection, computerSelection));
+//   }
+//   if (playerScore > compScore) {
+//     console.log("WELL DONE, YOU WIN THE GAME!!");
+//   } else if (compScore > playerScore) {
+//     console.log("BAD LUCK, COMPUTER WINS THE GAME");
+//   }
+// }
+
+// game();
