ebc0dbf5be3ad3d1fda0d781e28fcc467a977302
Stephen <stephensouness@outlook.com>
2023-07-27 18:44:55 +0100

    CSS basic styling - items aligned with flex

--- script.js
+++ script.js
@@ -5,11 +5,6 @@ function getComputerChoice() {
   return answer;
 }
 
-let playerScore = document.querySelector(".player-score");
-let compScore = document.querySelector(".comp-score");
-const result = document.querySelector(".result");
-const winner = document.querySelector(".winner");
-
 let playerCount = 0;
 let compCount = 0;
 
@@ -43,21 +38,37 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+let playerScore = document.querySelector(".player-score");
+let compScore = document.querySelector(".comp-score");
+let result = document.querySelector(".result");
+let winner = document.querySelector(".winner");
+let modal = document.querySelector(".modal-result");
+let modalContent = document.querySelector(".modal-content");
+let resetButton = document.querySelector(".reset-btn");
+
+modal.style.display = "none";
+
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     let playerSelection = button.id;
     let computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
-    if (playerCount === 5) {
-      alert((winner.innerText = "Well done, you are the winner!"));
-      restart();
-    } else if (compCount === 5) {
-      alert((winner.innerText = "Sorry you lose"));
-      restart();
-    }
+    gameWinner();
   });
 });
+
+function gameWinner() {
+  if (playerCount >= 5) {
+    modal.style.display = "block";
+    modalContent.innerText = "Well done, you are the winner!";
+  } else if (compCount >= 5) {
+    modal.style.display = "block";
+    modalContent.innerText = "Sorry you lose";
+  }
+}
+
+resetButton.addEventListener("click", restart);
 
 function restart() {
   playerCount = 0;
@@ -66,8 +77,7 @@ function restart() {
   compScore.innerText = compCount;
   result.innerText = "";
   winner.innerText = "";
+  result.innerText = "ROCK PAPER SCISSORS: FIRST TO 5 WINS";
+  modalContent.innerText = "";
+  modal.style.display = "none";
 }
-
-// SHould the winner message make everything else disappear??
-// Need a reset button!
-// Can probably remove the code from the buttons reset bit and apply it to a reset button
